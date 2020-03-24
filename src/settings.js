92fb8b3bb67319f449b9e0c00761e50ca1bb48ff
jyosisi <xu_zizi@163.com>
2020-03-24 10:55:29 +0800

    test

---
+++ src/settings.js
@@ -0,0 +1,44 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    launchScene: "db://assets/Scene/helloworld.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/helloworld.fire",
+            uuid: "2dL3kvpAxJu6GJ7RdqJG5J"
+        },
+        {
+            url: "db://assets/test.fire",
+            uuid: "ecgFl6ANxL1LbdaY2BFGVA"
+        }
+    ],
+    packedAssets: {
+        "05809a027": [
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            "ecgFl6ANxL1LbdaY2BFGVA"
+        ],
+        "05dd1dc0e": [
+            "2dL3kvpAxJu6GJ7RdqJG5J",
+            "31vIlawANFZqnzLlSuHBfc",
+            "41D7kWhyFGY7q4NDlzkazn"
+        ],
+        "0695875db": [
+            "02delMVqdBD70a/HSD99FK",
+            "6aoKpq6+5BVaCIpoemqt7E",
+            "a8Anh32NZGRZegUtSgEj26"
+        ]
+    },
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
